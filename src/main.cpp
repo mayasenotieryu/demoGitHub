234aee5c4a81ebf2f332e21ba823e6ff5d964569
mayasenotieryu <maya.snt.yu@gmail.com>
2026-01-26 15:00:40 +0100

    exemple 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,29 @@
 #include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+void maTache(void *parametres)
+{
+ while (1) // boucle infinie
+ {
+ Serial.printf("%s %08X\n", pcTaskGetName(NULL),
+ (int)xTaskGetCurrentTaskHandle());
+ delay(500);
+ }
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello, world!");
-  delay(1000);
+void setup()
+{
+ Serial.begin(115200);
+ while (!Serial);
+ Serial.printf("Départ\n");
+ xTaskCreate(
+ maTache, /* Fonction de la tâche. */
+ "Ma tâche", /* Nom de la tâche. */
+ 10000, /* Taille de la pile de la tâche */
+ NULL, /* Paramètres de la tâche, NULL si pas de paramètre */
+ 1, /* Priorité de la tâche */
+ NULL); /* Pointeur pour récupérer le « handle » de la tâche, optionnel */
+}
+void loop()
+{
+ Serial.printf("%s %08X\n", pcTaskGetName(NULL),
+ (int)xTaskGetCurrentTaskHandle());
+ delay(1000); 
 }
