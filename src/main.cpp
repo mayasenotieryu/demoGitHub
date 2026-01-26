46c824d850730d6e2508d4b08f417114af92624a
mayasenotieryu <maya.snt.yu@gmail.com>
2026-01-26 17:31:14 +0100

    exemple2

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,58 @@
 #include <Arduino.h>
-void maTache(void *parametres)
+
+TaskHandle_t hmaTache1 = NULL;
+TaskHandle_t hmaTache2 = NULL;
+
+void maTache1(void *parametres)
 {
- while (1) // boucle infinie
- {
- Serial.printf("%s %08X\n", pcTaskGetName(NULL),
- (int)xTaskGetCurrentTaskHandle());
- delay(500);
- }
+    int i = 0;
+    while (1) // boucle infinie
+    {
+        Serial.printf("maTache1 %4d\n", i++);
+        delay(500);
+        if (i = 5)
+            vTaskSuspend(hmaTache1);
+    }
+}
+void maTache2(void *parametres)
+{
+    int i = 0;
+    while (1) // boucle infinie
+    {
+        Serial.printf("maTache2 %4d\n", i++);
+        delay(2000);
+        if (i >= 10)
+            vTaskDelete(hmaTache2);
+    }
 }
 void setup()
 {
- Serial.begin(115200);
- while (!Serial);
- Serial.printf("Départ\n");
- xTaskCreate(
- maTache, /* Fonction de la tâche. */
- "Ma tâche", /* Nom de la tâche. */
- 10000, /* Taille de la pile de la tâche */
- NULL, /* Paramètres de la tâche, NULL si pas de paramètre */
- 1, /* Priorité de la tâche */
- NULL); /* Pointeur pour récupérer le « handle » de la tâche, optionnel */
+    Serial.begin(115200);
+    while (!Serial)
+        ;
+    Serial.printf("Départ\n");
+    xTaskCreate(
+        maTache1,     /* Fonction de la tâche. */
+        "Ma tâche 1", /* Nom de la tâche. */
+        10000,        /* Taille de la pile de la tâche */
+        NULL,         /* Paramètres de la tâche, NULL si pas de paramètre */
+        1,            /* Priorité de la tâche */
+        NULL);        /* Pointeur pour récupérer le « handle » de la tâche,
+              optionnel */
+    xTaskCreate(
+        maTache2,     /* Fonction de la tâche. */
+        "Ma tâche 2", /* Nom de la tâche. */
+        10000,        /* Taille de la pile de la tâche */
+        NULL,         /* Paramètres de la tâche, NULL si pas de paramètre */
+        1,            /* Priorité de la tâche */
+        NULL);        /* Pointeur pour récupérer le « handle » de la tâche,
+              optionnel */
 }
 void loop()
 {
- Serial.printf("%s %08X\n", pcTaskGetName(NULL),
- (int)xTaskGetCurrentTaskHandle());
- delay(1000); 
+    static int i = 0;
+    Serial.printf("Loop %4d\n", i++);
+    delay(1000);
+    if (i >= 15)
+        vTaskResume(hmaTache1);
 }
